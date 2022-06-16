7c4181bb9886962884b28e4771b861afb0fe796d
Sarah Mukuti <cesarwrites24@gmail.com>
2022-06-16 14:49:03 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    // your code here
+    //get all DOM elements
+    const taskForm = document.getElementById('create-task-form')
+    const taskDescription = document.getElementById('new-task-description')
+    taskForm.className = 'form-style';
+    
+    taskForm.addEventListener('submit', (e)=>{
+      e.preventDefault();
+      const newTask = document.createElement("li");
+      newTask.innerText = taskDescription.value;
+
+     function removeElement(){
+      const btn = document.createElement('button')
+      btn.innerText = ' X';
+      btn.style.backgroundColor = 'grey';
+      btn.className = 'btn-style';
+      newTask.appendChild(btn);
+      btn.addEventListener('click', (e) =>{
+        newTask.remove()
+      })
+     }
+      appendNewTask(newTask);
+      removeElement();
+      e.target.reset()
+    })
+  });
+  
+  const appendNewTask = task => {
+    document.getElementById("tasks").appendChild(task);
+  }
